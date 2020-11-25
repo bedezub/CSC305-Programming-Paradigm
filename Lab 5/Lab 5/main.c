5880cd8e32f94fc0f66117f367519b52c5f1c794
Quzaini Zubli <zubquzaini@Quzainis-Air.realtek>
2020-11-25 20:09:17 +0800

    Add c exercise

---
+++ Lab 5/Lab 5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab 5
+//
+//  Created by Zubli Quzaini Bin Zubli on 21/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
